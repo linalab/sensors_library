bb9d7eec6f15cf0a249e42386a8d7f1335f5fffa
turbulente <citlali.hs@gmail.com>
2024-05-17 17:50:17 +0200

    Update electret.ino

--- electret_microphone/electret/electret.ino
+++ electret_microphone/electret/electret.ino
@@ -1,24 +1,31 @@
-int LEDS = 3; 
-int pinA = A1; 
-int val;
+int miLed = 13;  // pin conectado a un led
+int pinA = A0;   // conectado a un pin analógico
+int pinD = 4;    // Conectado a un pin digital
+int valA;        // variable que cambiara entre 0 y 1 de acuerdo al umbral del potenciómetro del micro
+int valD;        // variable que cambiará entre 0 y 1023 de acuerdo al ruido
 
-void setup(){
-  Serial.begin(9600);
-  pinMode(LEDS, OUTPUT);
-  pinMode(sensor , INPUT_PULLUP); 
-  digitalWrite(LEDS, LOW); // 
+void setup() {
+  Serial.begin(9600);       // inicializamos la conexión serial entre ordenador y Arduino
+  pinMode(miLed, OUTPUT);   // presentamos al pin del led como OUTPUT
+  pinMode(sensorA, INPUT);  // presentamos al pin digital del sensor como INPUT
+  pinMode(sensorD, INPUT);  // presentamos al pin analógico del sensor como INPUT
+  digitalWrite(miLed, LOW);  // comenzamos el código con el led apagado
 }
 
-void loop(){
-  val = analogRead(pinA); // reads the value of the sensor
+void loop() {
+  val = analogRead(pinA);  // leemos el valor analógico del micro
+  sensorD = digitalRead(sensor);  // leemos el valor digital del micro
+  Serial.print("Pin Analógico= ");
+  Serial.println(val);// imprimimos el valor digital del micro
+  Serial.print("Pin Digital= ");
+  Serial.println(sensorD); // imprimimos el valor digital del micro
+  delay(200);
 
-  if ( val >= 700 ){ // if it's higher than 700 then
-    digitalWrite(LEDS, HIGH); // turn on the leds 
-    delay (1000);
-  }
-  else{
-    digitalWrite(LEDS, LOW); 
-  }
 
-  Serial.println(val);
+  if (val >= 600) {             // si el ruido supera el umbral de 600 ...
+    digitalWrite(miLed, HIGH);  // entonces led on
+    delay(1000);
+  } else { // si no supera el umbral ... 
+    digitalWrite(miLed, LOW); // entonces led off
+  }
 }
