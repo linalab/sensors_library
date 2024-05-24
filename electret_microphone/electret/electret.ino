74ce73ab9ce5f1f0dc7fadadffeab4df787ca991
linalab <linalab@hotmail.com>
2024-05-24 10:24:53 +0200

    Update electret.ino

--- electret_microphone/electret/electret.ino
+++ electret_microphone/electret/electret.ino
@@ -1,24 +1,18 @@
 int miLed = 13;  // pin conectado a un led
-int pinA = A0;   // conectado a un pin analógico
-int pinD = 4;    // Conectado a un pin digital
-int valA;        // variable que cambiara entre 0 y 1 de acuerdo al umbral del potenciómetro del micro
-int valD;        // variable que cambiará entre 0 y 1023 de acuerdo al ruido
+int pin = A0;   // conectado a un pin analógico
+int val;        // variable que cambiará entre 0 y 1023 de acuerdo al ruido
 
 void setup() {
   Serial.begin(9600);       // inicializamos la conexión serial entre ordenador y Arduino
   pinMode(miLed, OUTPUT);   // presentamos al pin del led como OUTPUT
-  pinMode(pinA, INPUT);  // presentamos al pin digital del sensor como INPUT
-  pinMode(pinD, INPUT);  // presentamos al pin analógico del sensor como INPUT
+  pinMode(pin, INPUT);  // presentamos al pin digital del sensor como INPUT
   digitalWrite(miLed, LOW);  // comenzamos el código con el led apagado
 }
 
 void loop() {
-  val = analogRead(pinA);  // leemos el valor analógico del micro
-  sensorD = digitalRead(sensor);  // leemos el valor digital del micro
+  val = analogRead(pin);  // leemos el valor analógico del micro
   Serial.print("Pin Analógico= ");
   Serial.println(val);// imprimimos el valor digital del micro
-  Serial.print("Pin Digital= ");
-  Serial.println(sensorD); // imprimimos el valor digital del micro
   delay(200);
 
 
